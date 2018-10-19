c1e6cc72a28e257ff89540107f352abb71ad1c48
Siphiwe-Manda <siphiwemanda@gmail.com>
2018-10-19 14:38:56 +0100

    Separating pages

---
+++ src/comp/pages/mainpage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Mainpage extends React.Component{
+  render(){
+    return(
+      <div>
+      <p>mainPage and all that jazz</p>
+      </div>
+    );
+  }
+}
+
+export default Mainpage;
