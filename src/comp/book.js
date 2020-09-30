a42478fbecbb28494821d06a46ae5cb24a7b4219
Siphiwe <siphiwemanda@gmail.com>
2020-09-30 07:31:37 +0100

    removed yellow book cover

--- src/comp/book.js
+++ src/comp/book.js
@@ -10,20 +10,8 @@ class Book extends Component{
         <div className="book-cover" style={{
           width: 128,
           height: 174,
-          backgroundImage: `url("${yellow}")`}}>
+          backgroundImage: `url("${this.props.book["book cover"]}")`}}>
         </div>
-        {/*
-        <div className="book-shelf-changer">
-            <select value = {this.props.book.shelf || "none"}
-            onChange={(e) => {this.props.updatebook(this.props.book, e.target.value)}}>
-            <option value="move" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
-        </div>
-        */}
       </div>
       <div className="book-title" >
       {this.props.book.Title}
