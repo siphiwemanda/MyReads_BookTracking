ba7e6a5258ebd261f42c1058ed45adaca242f146
Siphiwe <siphiwemanda@gmail.com>
2020-10-20 12:16:14 +0100

    Added on book API call

--- src/BooksAPI.js
+++ src/BooksAPI.js
@@ -3,8 +3,6 @@ const BannedBooksApi = "https://bannedbooks.herokuapp.com"
 export const allBooks = () =>
   fetch(`${BannedBooksApi}/book`).then(response => response.json()).then(data => data.books)
 
-
-
 export const oneBooks = (book) =>
   fetch(`${BannedBooksApi}/book/${book}`).then(response => response.json()).then(data => data)
 
